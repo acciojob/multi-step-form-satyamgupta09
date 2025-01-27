dd23d89440736a0f9f84a26f20dc021854f8de7c
satyamgupta09 <138917027+satyamgupta09@users.noreply.github.com>
2025-01-28 01:46:59 +0530

    Create Form1.js

---
+++ src/components/Form1.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+export default function Form1({ pageNumber, setPageNumber }) {
+  return (
+    <div>
+      <input type="text" id="model" placeholder="Model" />
+      <input type="number" id="car_price" placeholder="Price" />
+      <button
+        onClick={() => setPageNumber((prevPageNumber) => prevPageNumber - 1)}
+      >
+        Previous
+      </button>
+      <button
+        onClick={() => setPageNumber((prevPageNumber) => prevPageNumber + 1)}
+      >
+        Next
+      </button>
+    </div>
+  );
+}
