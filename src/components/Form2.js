051da5a8175fcff9b1b9868b0a88545acf1f1dd0
satyamgupta09 <138917027+satyamgupta09@users.noreply.github.com>
2025-01-28 01:46:22 +0530

    Create Form2.js

---
+++ src/components/Form2.js
@@ -0,0 +1,20 @@
+import React from "react";
+export default function Form2({ pageNumber, setPageNumber }) {
+  return (
+    <div>
+      <input
+        type="number"
+        id="card_info"
+        maxLength="12"
+        placeholder="Credit Card Number"
+      />
+      <input type="text" id="expiry_date" placeholder="Expiry Date" />
+      <button
+        onClick={() => setPageNumber((prevPageNumber) => prevPageNumber - 1)}
+      >
+        Previous
+      </button>
+      <button>Submit</button>
+    </div>
+  );
+}
