043a223b0f967c8c6a24aada8b472393d4d720fc
satyamgupta09 <138917027+satyamgupta09@users.noreply.github.com>
2025-02-04 00:50:22 +0530

    Update Form1.js

--- src/components/Form1.js
+++ src/components/Form1.js
@@ -5,16 +5,33 @@ export default function Form1({ pageNumber, setPageNumber }) {
     <div id="step2">
       <input type="text" id="model" placeholder="Model" />
       <input type="number" id="car_price" placeholder="Price" />
-      <button
-        onClick={() => setPageNumber((prevPageNumber) => prevPageNumber - 1)}
-      >
-        Previous
-      </button>
-      <button
-        onClick={() => setPageNumber((prevPageNumber) => prevPageNumber + 1)}
-      >
-        Next
-      </button>
+      // <button
+      //   onClick={() => setPageNumber((prevPageNumber) => prevPageNumber - 1)}
+      // >
+      //   Previous
+      // </button>
+      // <button
+      //   onClick={() => setPageNumber((prevPageNumber) => prevPageNumber + 1)}
+      // >
+      //   Next
+      // </button>
+    <button
+  onClick={(e) => {
+    e.preventDefault(); // Prevent form submission
+    setPageNumber((prevPageNumber) => prevPageNumber - 1);
+  }}
+>
+  Previous
+</button>
+<button
+  onClick={(e) => {
+    e.preventDefault(); // Prevent form submission
+    setPageNumber((prevPageNumber) => prevPageNumber + 1);
+  }}
+>
+  Next
+</button>
+
     </div>
   );
 }
