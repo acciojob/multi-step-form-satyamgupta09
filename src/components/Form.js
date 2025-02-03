b160048bf5dfa506cf3ae47448da9b2048b321aa
satyamgupta09 <138917027+satyamgupta09@users.noreply.github.com>
2025-02-04 00:51:18 +0530

    Update Form.js

--- src/components/Form.js
+++ src/components/Form.js
@@ -6,11 +6,20 @@ export default function Form({ pageNumber, setPageNumber }) {
       <form>
         <input type="text" id="first_name" placeholder="First Name" />
         <input type="text" id="last_name" placeholder="Last Name" />
-        <button
-          onClick={() => setPageNumber((prevPageNumber) => prevPageNumber + 1)}
-        >
-          Next
-        </button>
+        // <button
+        //   onClick={() => setPageNumber((prevPageNumber) => prevPageNumber + 1)}
+        // >
+        //   Next
+        // </button>
+    <button
+  onClick={(e) => {
+    e.preventDefault(); // Prevent form submission
+    setPageNumber((prevPageNumber) => prevPageNumber + 1);
+  }}
+>
+  Next
+</button>
+
       </form>
     </div>
   );
